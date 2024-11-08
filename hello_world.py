4656b7deaf6608578dcd3b2e0d459836f19b7f29
krutto89 <sitb01-028692022@student.mmust.ac.ke>
2024-11-08 16:35:34 +0300

    hello_world add and yml modification

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import datetime
+
+def main():
+    print("Hello World!")
+
+    current_time = datetime.datetime.now()
+    print("Current date and time: ", current_time)
+
+
+if __name__ == "__main__":
+    main()
